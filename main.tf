f522a2b74a421ebc6b7c4857c58e405843536d06
pkedar2 <pranavkear18@gmail.com>
2026-01-11 16:43:16 +0530

    added az

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ resource "aws_vpc" "terraform" {
 resource "aws_subnet" "terraform_subnet1" {
   vpc_id            = aws_vpc.terraform.id
   cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
