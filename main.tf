e3c8d76db8335959e318452a89db1aa6b1ec798e
Pranav Kedar <158163280+pkedar2@users.noreply.github.com>
2026-01-20 13:55:58 +0000

    outputs modified

--- main.tf
+++ main.tf
@@ -152,11 +152,6 @@ resource "aws_security_group" "sg" {
 
 # Outputs
 
-
-output "ec2_public_ip" {
-  value = aws_instance.eks.public_ip
-}
-
 output "eks_cluster_name" {
   value = aws_eks_cluster.eks_cluster.name
 }
